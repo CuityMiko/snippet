066b2051e598b96fafbb5c032fe4371973e99f54
qiu-deqing <deqing.qiu@gmail.com>
2015-07-29 18:25:49 +0800

    update swap-dom

--- swap-dom.js
+++ swap-dom.js
@@ -1,12 +1,9 @@
 /**
  * 交换两个DOM节点,需要保证他们都有父节点
- * @param a {Node} 需要交换的节点a
- * @param b {Node} 需要交换的节点b
+ * @param a {Element} 需要交换的节点a
+ * @param b {Element} 需要交换的节点b
  **/
 function swapDOM(a, b) {
-  var pa = a.parentNode;
-  var pb = b.parentNode;
-
   // 插入空白,保证具有nextSibling
   a.insertAdjacentHTML('afterend', ' ');
   b.insertAdjacentHTML('afterend', ' ');
@@ -14,6 +11,6 @@ function swapDOM(a, b) {
   var aSibling = a.nextSibling;
   var bSibling = b.nextSibling;
 
-  pa.insertBefore(b, aSibling);
-  pb.insertBefore(a, bSibling);
+  aSibling.parentNode.insertBefore(b, aSibling);
+  bSibling.parentNode.insertBefore(a, bSibling);
 }
